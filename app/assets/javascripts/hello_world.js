15cdd5d748cd8f1adcafe604f8e1f034305f10bd
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2015-09-14 00:34:56 -0300

    Corrigindo bug de carregamento do scripts

--- app/assets/javascripts/hello_world.js
+++ app/assets/javascripts/hello_world.js
@@ -1,23 +1,19 @@
-var helloWorld = setInterval(function() {
-	if (typeof ChatReact !== "undefined") {
-		var chat = new ChatReact([
-			{
-				author : {
-					nome : "Daniele Batista"
-				},
-				texto : "Olá Pedro! Tudo bem?",
-				dt_envio : new Date("2015-09-13 23:21 GMT-0300")
-			},
-			{
-				author : {
-					nome : "Pedro Marcelo"
-				},
-				texto : "Tudo sim! E você?",
-				dt_envio : new Date("2015-09-13 23:23 GMT-0300")
-			}
-		]);
-
-		chat.render('test-cr');
-		clearInterval(helloWorld);
+var chat = new ChatReact([
+	{
+		author : {
+			nome : "Daniele Batista"
+		},
+		texto : "Olá Pedro! Tudo bem?",
+		dt_envio : new Date("2015-09-13 23:21 GMT-0300")
+	},
+	{
+		author : {
+			nome : "Pedro Marcelo"
+		},
+		texto : "Tudo sim! E você?",
+		dt_envio : new Date("2015-09-13 23:23 GMT-0300")
 	}
-}, 1000);
+]);
+
+chat.render('test-cr');
+clearInterval(helloWorld);
